7d277d07ba8d95da9ca0277e81f12cf6707c6f6a
Ishaan Ohri <ishaan99ohri@gmail.com>
2020-11-14 04:11:16 +0530

    Change port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const moment = require("moment");
 const mysql = require("mysql2");
 
 const app = express();
-const PORT = 80;
+const PORT = 3000;
 
 const connection = mysql.createConnection({
   host: "localhost",
