aa899ecf1364de5c3afe57df72c9b9dc9ff1c306
Ishaan Ohri <ishaan99ohri@gmail.com>
2020-11-14 00:18:32 +0530

    Added check for empty name

--- client/app.js
+++ client/app.js
@@ -1,24 +1,27 @@
 $(document).ready(function () {
-    $("#view").click(function () {
-      window.location.href='./entries.html';
-    });
+  $("#view").click(function () {
+    window.location.href = "./entries.html";
+  });
 
-    $("#submit").click(function () {
-      var name = $("#name").val().toUpperCase();
-      
+  $("#submit").click(function () {
+    var name = $("#name").val().toUpperCase();
+
+    if (name === "") {
+      alert("Please enter a name");
+    } else {
       var settings = {
-        "url": `/add?name=${name}`,
-        "method": "GET",
-        "timeout": 0,
+        url: `/add?name=${name}`,
+        method: "GET",
+        timeout: 0,
       };
-      
+
       $.ajax(settings).done(function (response) {
-        if(response.success){
-          alert('Data entered successfully!');
-        }else{
-          alert('Data could not be entered in database. Try again.');
+        if (response.success) {
+          alert("Data entered successfully!");
+        } else {
+          alert("Data could not be entered in database. Try again.");
         }
       });
-    });
+    }
   });
-  
+});
